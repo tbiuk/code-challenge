6e13451beb3fb95a361ae064ac171ba63f721424
tbiuk <toni.biuk@gmail.com>
2022-12-24 12:44:14 +0100

    feat: authentication guard

--- code-challenge-frontend/src/app/app-routing.module.ts
+++ code-challenge-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EncoderComponent } from './_components/encoder/encoder.component';
 import { LoginComponent } from './_components/login/login.component';
+import { AuthGuardService } from './_services/auth-guard/auth-guard.service';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'encoder',
+    component: EncoderComponent,
+    canActivate: [AuthGuardService],
+  },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
