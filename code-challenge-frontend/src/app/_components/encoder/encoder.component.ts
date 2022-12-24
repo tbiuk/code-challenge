2608bb28e0e801b64d4056bc0defd68200ce7fdb
tbiuk <toni.biuk@gmail.com>
2022-12-24 12:41:01 +0100

    chore: generate encoder component

---
+++ code-challenge-frontend/src/app/_components/encoder/encoder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encoder',
+  templateUrl: './encoder.component.html',
+  styleUrls: ['./encoder.component.scss']
+})
+export class EncoderComponent {
+
+}
--- code-challenge-frontend/src/app/app.module.ts
+++ code-challenge-frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './_components/login/login.component';
+import { EncoderComponent } from './_components/encoder/encoder.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, EncoderComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
