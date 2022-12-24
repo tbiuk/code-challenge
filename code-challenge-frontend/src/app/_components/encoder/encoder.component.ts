a0d56ce22c91e7728ad2a08df538c4c4d19e410f
tbiuk <toni.biuk@gmail.com>
2022-12-24 17:21:08 +0100

    feat: encoder component

--- code-challenge-frontend/src/app/_components/encoder/encoder.component.ts
+++ code-challenge-frontend/src/app/_components/encoder/encoder.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpService } from 'src/app/_services/http/http.service';
 
 @Component({
   selector: 'app-encoder',
   templateUrl: './encoder.component.html',
-  styleUrls: ['./encoder.component.scss']
+  styleUrls: ['./encoder.component.scss'],
 })
 export class EncoderComponent {
+  input: string = '';
+  result: string = '';
+  isError: boolean = false;
 
+  constructor(private httpService: HttpService) {}
+
+  async getResult(input: string) {
+    const { result, isError } = await this.httpService.sendEncoderRequest(
+      input
+    );
+    this.isError = isError;
+    this.result = result;
+  }
 }
--- code-challenge-frontend/src/app/app.module.ts
+++ code-challenge-frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
